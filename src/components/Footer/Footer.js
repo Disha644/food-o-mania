ba6b41bb59fcac7e12f3898f39fe71ea76f16c0a
Disha644 <dgupta644@gmail.com>
2021-04-20 23:16:42 +0530

    Footer Completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,16 @@ import classes from './Footer.css';
 const Footer = (props) => {
     return (
         <footer className={classes.Footer}>
-            <i class="fab fa-google-play"></i>
-            <p>Made with love in India</p>
+
+            <i class="fab fa-twitter footer-icons"></i>
+            <i class="fab fa-facebook-f footer-icons"></i>
+            <i class="fab fa-instagram footer-icons"></i>
+            <i class="fas fa-envelope footer-icons"></i>
+
+            <p>Made with <i class="far fa-heart" style={{ color: 'rgb(253, 75, 75)', margin: 'auto' }}></i> in India</p>
+            <p>All the <i class="far fa-copyright" style={{ fontSize: 'small', margin: 'auto' }}></i> Copyrights are reserved
+            Food-o-Mania</p>
+
         </footer>
     );
 }
